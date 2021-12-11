69dd1d239638d53c6ada777cfa0e739dd0e7e884
AHMED <med.informat@gmail.com>
2021-12-11 14:18:27 +0100

    js new

--- Script/app.js
+++ Script/app.js
@@ -1,38 +1,48 @@
-function generateSum(n1, n2) {
-  var sum = n1 + n2;
-  console.log(sum);
-}
+// function generateSum(n1, n2) {
+//   var sum = n1 + n2;
+//   console.log(sum);
+// }
 
-generateSum(7, 2);
+// generateSum(7, 2);
 
-// conditionner pour que les deux n soit numbers
+// // conditionner pour que les deux n soit numbers
 
-function generateSum(n1, n2, n3) {
-  if (typeof n1 == "number") {
-    if (typeof n2 == "number") {
-      if (typeof n3 == "number") {
-      } else {
-        console.log("n3 is not numer");
-      }
-      var sum = n1 + n2 + n3;
-      console.log(sum);
-    } else {
-      console.log("n2 must be number");
-    }
-  } else {
-    console.log("n1 must be number");
+// function generateSum(n1, n2, n3) {
+//   if (typeof n1 == "number") {
+//     if (typeof n2 == "number") {
+//       if (typeof n3 == "number") {
+//       } else {
+//         console.log("n3 is not numer");
+//       }
+//       var sum = n1 + n2 + n3;
+//       console.log(sum);
+//     } else {
+//       console.log("n2 must be number");
+//     }
+//   } else {
+//     console.log("n1 must be number");
+//   }
+// }
+// generateSum(7, 2, 6);
+
+// // conditionner pour que les deux n soit string
+
+// function generatFullname(FirstName, LastName) {
+//   if (typeof FirstName == "string" && typeof LastName == "string") {
+//     var fullName = FirstName + " " + LastName;
+//     console.log(fullName)
+//   } else {
+//     console.log("first or last name should be string");
+//   }
+// }
+// generatFullname("ahmed", "ait si said");
+
+function printIamDev () {
+  for (var i = 1 ; i <= 3 ; i ++ ){
+    var b = b + i;
+    console.log(b);
   }
 }
-generateSum(7, 2, 6);
+printIamDev()
 
-// conditionner pour que les deux n soit string
 
-function generatFullname(FirstName, LastName) {
-  if (typeof FirstName == "string" && typeof LastName == "string") {
-    var fullName = FirstName + " " + LastName;
-    console.log(fullName)
-  } else {
-    console.log("first or last name should be string");
-  }
-}
-generatFullname("ahmed", "ait si said");
