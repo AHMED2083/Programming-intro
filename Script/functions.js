5f155f2750c4f5a5fe42048cb165898a5cab305e
AHMED <med.informat@gmail.com>
2021-12-11 16:03:13 +0100

    EXO LOGIN

--- Script/functions.js
+++ Script/functions.js
@@ -1,1 +1,26 @@
-console.log("Hello AÏT SI SAÏD");
+// function login(User, PassWord) {
+//   if (User == "ahmed" && PassWord == 123) {
+//     console.log("succeful");
+//   } else {
+//     console.log("error");
+//   }
+// }
+// login("ahmed", 123);
+
+var originalUesrName = "AHMED";
+var originalPassWord = 1234;
+
+function login(UesrName, PassWord) {
+  if (UesrName == originalUesrName) {
+    if (PassWord == originalPassWord) {
+      console.log("login succefuly");
+    } else {
+      console.log("Wrong password");
+    }
+  } else {
+    console.log("not exist in DB");
+  }
+}
+login("sdhjsdh", 123);
+login("AHMED", 23468);
+login("AHMED", 1234);
