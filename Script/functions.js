4fd4dbd626595491a8cdc73a9ccb4f0cffd96fd5
AHMED <med.informat@gmail.com>
2021-12-17 14:53:05 +0100

    2*2

--- Script/functions.js
+++ Script/functions.js
@@ -7,37 +7,50 @@
 // }
 // login("ahmed", 123);
 
-var originalUesrName = "AHMED";
-var originalPassWord = 1234;
+// var originalUesrName = "AHMED";
+// var originalPassWord = 1234;
 
-function login(UesrName, PassWord) {
-  if (UesrName == originalUesrName) {
-    if (PassWord == originalPassWord) {
-      console.log("login succefuly");
-    } else {
-      console.log("Wrong password");
-    }
-  } else {
-    console.log("not exist in DB");
-  }
-}
-login("sdhjsdh", 123);
-login("AHMED", 23468);
-login("AHMED", 1234);
+// function login(UesrName, PassWord) {
+//   if (UesrName == originalUesrName) {
+//     if (PassWord == originalPassWord) {
+//       console.log("login succefuly");
+//     } else {
+//       console.log("Wrong password");
+//     }
+//   } else {
+//     console.log("not exist in DB");
+//   }
+// }
+// login("sdhjsdh", 123);
+// login("AHMED", 23468);
+// login("AHMED", 1234);
 
-// #4 moyenne
+// // #4 moyenne
 
-var studentList = [14, 11, 11, 10, 5];
+// var studentList = [14, 11, 11, 10, 5];
 
-function calculateMoy() {
-  var sum = 0;
-  for (var i = 0; i < studentList.length; i++) {
-    sum = sum + studentList[i];
-  }
-  var moy = sum / studentList.length;
-  console.log(moy);
+// function calculateMoy() {
+//   var sum = 0;
+//   for (var i = 0; i < studentList.length; i++) {
+//     sum = sum + studentList[i];
+//   }
+//   var moy = sum / studentList.length;
+//   console.log(moy);
   
+//   }
+// }
+
+
+function squar(input){
+  if (typeof input === "number") {
+    var result = input * input;
+    return result;
+  }else{
+    return ("input is not a number!!!!!");
   }
 }
 
-calculateMoy();
+var r1 = squar(2);
+var r2 = squar("Algeria")
+
+console.log(r1, r2);
